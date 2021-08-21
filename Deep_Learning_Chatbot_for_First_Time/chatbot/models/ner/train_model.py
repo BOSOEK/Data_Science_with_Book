02a5e028325c696a99502128332c0570d98a9a9d
BOSOEK <thydcj@naver.com>
2021-08-21 23:46:16 +0900

    NER 모델

--- Deep_Learning_Chatbot_for_First_Time/chatbot/models/ner/train_model.py
+++ Deep_Learning_Chatbot_for_First_Time/chatbot/models/ner/train_model.py
@@ -15,9 +15,9 @@ def read_file(file_name):
     with open(file_name, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         for idx, l in enumerate(lines):
-            if l[0] == ',' and lines[idx + 1][0] == '$':
+            if l[0] == ';' and lines[idx + 1][0] == '$':
                 this_sent = []
-            elif l[0] == '$' and lines[idx - 1][0] == ',':
+            elif l[0] == '$' and lines[idx - 1][0] == ';':
                 continue
             elif l[0] == '\n':
                 sents.append(this_sent)
@@ -26,10 +26,10 @@ def read_file(file_name):
     return sents
 
 # 전처리 객체 생성
-p = Preprocess(word2index_dix='./train_tools/dict/chatbot_dict.bin', userdic='./utils/user_dic.tsv')
+p = Preprocess(word2index_dic='./train_tools/dict/chatbot_dict.bin', userdic='./utils/user_dic.tsv')
 
 # 학습용 말뭉치 데이터를 불러옴
-corpus = read_file('models/ner/ner_train.txt')
+corpus = read_file('./models/ner/ner_train.txt')
 
 # 말뭉치 데이터에서 단어와 BIO 태그만 불러와 학습용 데이터셋 생성
 sentences, tags = [], []
@@ -70,3 +70,54 @@ index_to_ner[0] = 'PAD'
 # 시퀀스 패딩 처리
 max_len = 40
 x_train = preprocessing.sequence.pad_sequences(x_train, padding='post', maxlen=max_len)
+y_train = preprocessing.sequence.pad_sequences(y_train, padding='post', maxlen=max_len)
+
+# 학습 & 테스트 데이터 분리
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=.2, random_state=1234)
+
+# 출력 데이터를 원-핫 인코딩
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=tag_size)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=tag_size)
+
+print('학습 샘플 시퀀스 형상 : ', x_train.shape)
+print('학습 샘플 레이블 형상 : ', y_train.shape)
+print('테스트 샘플 시퀀스 형상 : ', x_test.shape)
+print('테스트 샘플 레이블 형상 : ', y_test.shape)
+
+# 모델 정의: Bi-LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+from tensorflow.keras.optimizers import Adam
+
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size, output_dim=30, input_length=max_len, mask_zero=True))
+model.add(Bidirectional(LSTM(200, return_sequences=True, dropout=0.50, recurrent_dropout=0.25)))
+model.add(TimeDistributed(Dense(tag_size, activation='softmax')))
+model.compile(loss='categorical_crossentropy', optimizer=Adam(0.01), metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=128, epochs = 10)
+
+print('모델 평가 : ', model.evaluate(x_test, y_test)[1])
+model.save('models/ner/ner_model.h5')
+
+# 시퀀스를 NER 태그로 변환
+def sequences_to_tag(sequences):    # 예측값을 index_to_ner로 태깅 정보로 변경하는 함수
+    result = []
+    for sequence in sequences:     # 전체 시퀀스로부터 시퀀스를 하나씩 꺼낸다.
+        temp = []
+        for pred in sequence:       # 시퀀스로부터 예측값을 하나씩 꺼낸다
+            pred_index = np.argmax(pred)    # 예로 [0, 0, 0, 1, 0]이면 1의 인덱스
+            temp.append(index_to_ner[pred_index].replace('PAD', '0'))   # PAD는 0으로 변경
+        result.append(temp)
+    return result
+
+# F1 스코어 계산
+from seqeval.metrics import f1_score, classification_report
+
+# 테스트 데이터셋의 NER 예측
+y_predicted = model.predict(x_test)
+pred_tags = sequences_to_tag(y_predicted)   # 예측 NER
+test_tags = sequences_to_tag(y_test)        # 실제 NER
+
+# F1 평가 결과
+print(classification_report(test_tags, pred_tags))
+print('F1-score : {:.1%}'.format(f1_score(test_tags, pred_tags)))
